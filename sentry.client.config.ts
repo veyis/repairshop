cfcdfb62ab2a235e416593f663c46d9f01b2b50a
veyis <vakgul@gmail.com>
2024-11-09 10:11:20 -0800

    db

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,23 +1,28 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://861a8eb10739da338427be315ced1f1c@o4508264365817856.ingest.us.sentry.io/4508264367128576",
-  // Replay may only be enabled for the client-side
-  integrations: [Sentry.replayIntegration()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for tracing.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
 
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
 
-  // ...
-
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 });
